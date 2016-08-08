f2e4e16f4c8e2e42861d5db4061de181628ef671
Kyle McNally <kyle@kmcnally.net>
2016-08-08 12:27:18 -0400

    add tablets boxstarter script

---
+++ tablets.ps1
@@ -0,0 +1,62 @@
+# START http://boxstarter.org/package/nr/url?
+
+$Boxstarter.RebootOk=$true
+$Boxstarter.NoPassword=$false
+$Boxstarter.AutoLogin=$true
+
+# Windows Stuff
+	Disable-BingSearch
+	Update-ExecutionPolicy RemoteSigned
+
+	tzutil /s "Eastern Standard Time"
+
+# Updates & Backend
+	choco install chocolatey -y
+	choco install powershell -y
+
+# Tools
+	choco install emet -y
+	choco install btsync -y
+	choco install followmee -y
+	choco install networx -y
+
+# Applications
+	choco install libreoffice -y
+	choco install skype -y
+	choco install slack -y
+	choco install zoom -y
+
+# Browsers
+	choco install googlechrome -y
+
+	Install-ChocolateyPinnedTaskBarItem "$env:localappdata\Google\Chrome\Application\chrome.exe"
+
+#	# Create Shortcuts
+#	$TargetFile = "$env:SystemRoot\System32\notepad.exe"
+#	$ShortcutFile = "$env:Public\Desktop\Notepad.lnk"
+#	$WScriptShell = New-Object -ComObject WScript.Shell
+#	$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+#	$Shortcut.TargetPath = $TargetFile
+#	$Shortcut.Save()
+
+#Install-ChocolateyShortcut `
+ # -ShortcutFilePath "C:\notepad.lnk" `
+  #-TargetPath "C:\Windows\System32\notepad.exe" `
+#  -WorkDirectory "C:\" `
+ # -Arguments "C:\test.txt" `
+  #-IconLocation "C:\test.ico" `
+#  -Description "This is the description"
+
+$Shell = New-Object -ComObject ("WScript.Shell")
+
+$App = $Shell.CreateShortcut($env:USERPROFILE + "\Desktop\DAZSER Web App.url")
+$App.TargetPath = "https://www.dazser.net"
+$App.Save()
+
+$Mail = $Shell.CreateShortcut($env:USERPROFILE + "\Desktop\Web Mail.url")
+$Mail.TargetPath = "https://mail.dazser.com"
+$Mail.Save()
+
+# Windows Stuff
+Enable-MicrosoftUpdate
+Install-WindowsUpdate -acceptEula
