33e739b440475a3ce6a4e40f9e13da2e87f49535
Kyle McNally <kyle@kmcnally.net>
2016-08-10 10:30:04 -0400

    Add BTSync config

---
+++ btsync.ps1
@@ -0,0 +1,12 @@
+# Get JSON data
+$jsonFileData = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Sparticuz/boxstarter-scripts/master/btsync.conf"
+$jsonObject = $jsonFileData | ConvertFrom-Json
+
+# Set JSON settings
+$jsonObject.device_name = $env:ComputerName
+$jsonObject.storage_path = $env:UserProfile+"\Documents"
+$jsonObject.shared_folders.dir = $env:UserProfile+"\Documents\Forms"
+
+# Write to Conf file
+$jsonFileDataToWrite = $jsonObject | ConvertTo-Json
+$jsonFileDataToWrite | Out-File $env:AppData+"\Bittorrent Sync\btsync.conf"
