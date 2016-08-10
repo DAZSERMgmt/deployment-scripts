36544a703dcc044f8001abfb6ae6357b0060372f
Kyle McNally <kyle@kmcnally.net>
2016-08-10 11:26:08 -0400

    test encoding change

--- btsync.ps1
+++ btsync.ps1
@@ -5,8 +5,8 @@ $jsonObject = $jsonFileData.Content | ConvertFrom-Json
 # Set JSON settings
 $jsonObject.device_name = $env:ComputerName
 $jsonObject.storage_path = $env:UserProfile+"\Documents"
-$jsonObject.shared_folders.dir = $env:UserProfile+"\Documents\Forms"
+$jsonObject.shared_folders[0].dir = $env:UserProfile+"\Documents\Forms"
 
 # Write to Conf file
 $jsonFileDataToWrite = $jsonObject | ConvertTo-Json
-$jsonFileDataToWrite | Out-File $env:AppData"\Bittorrent Sync\btsync.conf"
+$jsonFileDataToWrite | Out-File $env:AppData"\Bittorrent Sync\btsync.conf" -Encoding utf8
