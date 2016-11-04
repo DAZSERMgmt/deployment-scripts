3eda7a31955d53cf702eebb1362d1457bba23978
Kyle McNally <kyle@kmcnally.net>
2016-11-04 13:08:08 -0400

    boxstarter is giving me troubles, i'm dropping it for straight powershell

---
+++ install.ps1
@@ -0,0 +1,16 @@
+# Set execution policy
+$policy = Get-ExecutionPolicy
+if (!($policy -eq "RemoteSigned")) {
+    Write-Output "Policy is set to $policy, setting to RemoteSigned"
+    Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+}
+
+Write-Output "Pulling tablet.ps1"
+Invoke-WebRequest https://raw.githubusercontent.com/Sparticuz/boxstarter-scripts/master/tablet.ps1 -OutFile $env:temp\tablet.ps1 
+Write-Output  "Executing tablet.ps1"
+Start-Process powershell -ArgumentList "-noprofile", "-file ${env:temp}\tablet.ps1" -Verb RunAs -Wait
+
+# Cleanup after Process done
+Remove-Item $env:userprofile\Desktop\install.ps1
+Remove-Item $env:appdata\Microsoft\Windows\Start Menu\Programs\Startup\post-restart.bat
+Remove-Item $env:temp\tablet.ps1
--- tablet.ps1
+++ tablet.ps1
@@ -1,76 +1,108 @@
-# START http://boxstarter.org/package/url?
+# Start a new Tablet
+
+function Test-PendingReboot {
+  if (Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore) { return $true }
+  if (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) { return $true }
+  if (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -EA Ignore) { return $true }
+  try { 
+    $util = [wmiclass]"\\.\root\ccm\clientsdk:CCM_ClientUtilities"
+    $status = $util.DetermineIfRebootPending()
+    if(($status -ne $null) -and $status.RebootPending){
+      return $true
+    }
+  } catch {
+    Write-Error "PendingReboot Test failed"
+  }
+  return $false
+}
+
+function Invoke-Reboot {
+  #Need to create startup to call install.ps1 again
+  Write-Output "Writing Restart file"
+  $startup = "$env:appdata\Microsoft\Windows\Start Menu\Programs\Startup"
+  Invoke-WebRequest https://raw.githubusercontent.com/Sparticuz/boxstarter-scripts/master/install.ps1 -OutFile $env:USERPROFILE\Desktop\install.ps1
+  $restartScript = "powershell.exe -File '$env:USERPROFILE\Desktop\install.ps1'"
+  New-Item "$startup\post-restart.bat" -type file -force -value $restartScript | Out-Null
+
+  Restart-Computer -Force
+}
+
 
 # Windows Stuff
-	Disable-BingSearch
-	
-	$policy = Get-ExecutionPolicy
-	if (!($policy -eq "RemoteSigned")) {
-		Update-ExecutionPolicy RemoteSigned
-	}
-	
-	# Update the timezone and time
-	tzutil /s "Eastern Standard Time"
-	w32tm /resync /force
+  # Update the timezone and time
+  $time = tzutil /g
+  if (!($time -eq "Eastern Standard Time")) {
+    tzutil /s "Eastern Standard Time"
+  }
 
-	# Set computer name
-	If (!(Test-Path C:\computerNamed)) {
-		$name = Read-Host "What is your computer name?"
-		Rename-Computer -NewName $name
-		echo $name >> C:\computerNamed
-		if (Test-PendingReboot) { Invoke-Reboot }
-	}
+  $policy = Get-ExecutionPolicy
+  if (!($policy -eq "RemoteSigned")) {
+      Write-Output "Local Policy is set to $policy, setting to RemoteSigned"
+      Set-ExecutionPolicy RemoteSigned -Scope LocalMachine
+  }
+  
+  # Install chocolatey
+  iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+
+  # Set computer name
+  If (!(Test-Path C:\computerNamed)) {
+    $name = Read-Host "What is your computer name?"
+    Rename-Computer -NewName $name
+    echo $name >> C:\computerNamed
+    if (Test-PendingReboot) { Invoke-Reboot }
+  }
 
 # Updates & Backend
-	choco install chocolatey --source=chocolatey -y
-	choco install powershell --source=chocolatey -y
-	choco install javaruntime -y
+  choco install chocolatey --source=chocolatey -y
+  choco install powershell --source=chocolatey -y
+  choco install javaruntime -y
 
 # Tools
-	#choco install emet -y
-	# Configure EMET
-	#Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Sparticuz/boxstarter-scripts/master/EMET-Settings.xml" -OutFile ${Env:ProgramFiles(x86)}"\EMET 5.5\MyEMETSettings.xml"
-	#$path = ${Env:ProgramFiles(x86)}+"\EMET 5.5"
-	#& $path\EMET_Conf.exe --import $path\MyEMETSettings.xml
+  #choco install emet -y
+  # Configure EMET
+  #Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Sparticuz/boxstarter-scripts/master/EMET-Settings.xml" -OutFile ${Env:ProgramFiles(x86)}"\EMET 5.5\MyEMETSettings.xml"
+  #$path = ${Env:ProgramFiles(x86)}+"\EMET 5.5"
+  #& $path\EMET_Conf.exe --import $path\MyEMETSettings.xml
 
-	choco install btsync -y
-	# BTSync starts after install, so kill it.
-	Stop-Process -ProcessName btsync
-	# Next, run btsync.ps1 to generate btsync.conf
-	# Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Sparticuz/boxstarter-scripts/master/btsync.ps1" -UseBasicParsing | Invoke-Expression
-	# Run btsync
-	# $env:appdata+"\Bittorrent Sync\btsync.exe /config btsync.conf"
+  choco install btsync -y
+  # BTSync starts after install, so kill it.
+  Stop-Process -ProcessName btsync
+  # Next, run btsync.ps1 to generate btsync.conf
+  # Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Sparticuz/boxstarter-scripts/master/btsync.ps1" -UseBasicParsing | Invoke-Expression
+  # Run btsync
+  # $env:appdata+"\Bittorrent Sync\btsync.exe /config btsync.conf"
 
-	#choco install followmee -y
-	# Get FollowMee settings & Start the service
-	#Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Sparticuz/boxstarter-scripts/master/FollowMee-Settings.xml" -OutFile ${Env:ProgramFiles(x86)}"\FollowMee\Settings.xml"
-	#Start-Service FMEEService
+  #choco install followmee -y
+  # Get FollowMee settings & Start the service
+  #Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Sparticuz/boxstarter-scripts/master/FollowMee-Settings.xml" -OutFile ${Env:ProgramFiles(x86)}"\FollowMee\Settings.xml"
+  #Start-Service FMEEService
 
-	choco install networx -y
-	Stop-Process -ProcessName networx
-	# Now get the OpenSSL files
-	$file = "C:\openssl.zip"
-	Invoke-WebRequest -Uri "https://indy.fulgan.com/SSL/openssl-1.0.2j-x64_86-win64.zip" -OutFile $file
-	# Unzip the file to specified location
-	$shell_app = New-Object -Com Shell.Application 
-	$zip_file = $shell_app.namespace($file)
-	$path = $Env:ProgramFiles+"\Networx\"
-	$destination = $shell_app.namespace($path) 
-	$destination.Copyhere($zip_file.items())
-	Remove-Item $file
-	# Now get the settings database file
-	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Sparticuz/boxstarter-scripts/master/networx.db" -OutFile $Env:ProgramFiles"\NetWorx\NetWorx.db"
+  choco install networx -y
+  Stop-Process -ProcessName networx
+  # Now get the OpenSSL files
+  $file = "C:\openssl.zip"
+  Invoke-WebRequest -Uri "https://indy.fulgan.com/SSL/openssl-1.0.2j-x64_86-win64.zip" -OutFile $file
+  # Unzip the file to specified location
+  $shell_app = New-Object -Com Shell.Application 
+  $zip_file = $shell_app.namespace($file)
+  $path = $Env:ProgramFiles+"\Networx\"
+  $destination = $shell_app.namespace($path) 
+  $destination.Copyhere($zip_file.items())
+  Remove-Item $file
+  # Now get the settings database file
+  Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Sparticuz/boxstarter-scripts/master/networx.db" -OutFile $Env:ProgramFiles"\NetWorx\NetWorx.db"
 
 # Applications
-	choco install libreoffice -y
-	choco install skype -y
-	choco install slack -y
-	choco install zoom -y
+  choco install libreoffice -y
+  choco install skype -y
+  choco install slack -y
+  choco install zoom -y
 
 # Browsers
-	choco install googlechrome -y
-	# Copy master_preferences to Chrome profile
-	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Sparticuz/boxstarter-scripts/master/master_preferences" -OutFile ${Env:ProgramFiles(x86)}"\Google\Chrome\Application\master_preferences"
-	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Sparticuz/boxstarter-scripts/master/initialbookmarks.html" -OutFile ${Env:ProgramFiles(x86)}"\Google\Chrome\Application\initialbookmarks.html"
+  choco install googlechrome -y
+  # Copy master_preferences to Chrome profile
+  Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Sparticuz/boxstarter-scripts/master/master_preferences" -OutFile ${Env:ProgramFiles(x86)}"\Google\Chrome\Application\master_preferences"
+  Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Sparticuz/boxstarter-scripts/master/initialbookmarks.html" -OutFile ${Env:ProgramFiles(x86)}"\Google\Chrome\Application\initialbookmarks.html"
 
 #	# Create Shortcuts
 #	$TargetFile = "$env:SystemRoot\System32\notepad.exe"
@@ -101,12 +133,10 @@ $Mail.IconLocation = "$env:windir\System32\shell32.dll, 42"
 $Mail.Save()
 
 # Windows Stuff
-	#Show Powershell on Win+X instead of Command Prompt #kill explorer
-	Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name DontUsePowerShellOnWinX -Value 0
-	#File Explorer preferences
-	Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneExpandToCurrentFolder -Value 1
-	Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1
-	Enable-MicrosoftUpdate
-	Install-WindowsUpdate -acceptEula
+  #Show Powershell on Win+X instead of Command Prompt #kill explorer
+  Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name DontUsePowerShellOnWinX -Value 0
+  #File Explorer preferences
+  Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneExpandToCurrentFolder -Value 1
+  Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1
 
 Remove-Item C:\computerNamed
