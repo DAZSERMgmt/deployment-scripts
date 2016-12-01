387c5250a46d6e9f10cf72e9910db5fd0ebb4de6
Kyle McNally <kyle@kmcnally.net>
2016-12-01 14:16:49 -0500

    switch quotes

--- tablet.ps1
+++ tablet.ps1
@@ -132,14 +132,14 @@ function Invoke-Reboot {
 
 # Set up recurring powershell script
   mofcomp C:\Windows\System32\wbem\SchedProv.mof
-  $chocoCmd = Get-Command -Name 'choco' -ErrorAction SilentlyContinue -WarningAction SilentlyContinue | Select-Object -ExpandProperty Source
+  $chocoCmd = Get-Command -Name "choco" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue | Select-Object -ExpandProperty Source
 
   # Settings for the scheduled task
-  $taskAction = New-ScheduledTaskAction –Execute $chocoCmd -Argument 'upgrade all -y'
+  $taskAction = New-ScheduledTaskAction –Execute $chocoCmd -Argument "upgrade all -y"
   $taskTrigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Sunday -At 12pm
-  $taskUserPrincipal = New-ScheduledTaskPrincipal -UserId 'SYSTEM'
+  $taskUserPrincipal = New-ScheduledTaskPrincipal -UserId "SYSTEM"
   $taskSettings = New-ScheduledTaskSettingsSet -Compatibility Win8
 
   # Set up the task, and register it
   $task = New-ScheduledTask -Action $taskAction -Principal $taskUserPrincipal -Trigger $taskTrigger -Settings $taskSettings
-  Register-ScheduledTask -TaskName 'ChocoUpgrade' -InputObject $task -Force
+  Register-ScheduledTask -TaskName "ChocoUpgrade" -InputObject $task -Force
