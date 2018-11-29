4eb0791d3d2b86fb3791f89e39a670c3cc9ed01b
Kyle McNally <kyle@kmcnally.net>
2018-11-29 16:42:18 -0500

    Change the URL for the tablet script and remove more items

--- install.ps1
+++ install.ps1
@@ -6,10 +6,11 @@ if (!($policy -eq "RemoteSigned")) {
 }
 
 Write-Output "Pulling tablet.ps1"
-Invoke-WebRequest https://raw.githubusercontent.com/Sparticuz/boxstarter-scripts/master/tablet.ps1 -OutFile $env:temp\tablet.ps1 
+Invoke-WebRequest https://raw.githubusercontent.com/DAZSERMgmt/boxstarter-scripts/master/tablet.ps1 -OutFile $env:temp\tablet.ps1 
 Write-Output  "Executing tablet.ps1"
 Start-Process powershell -ArgumentList "-noprofile", "-file ${env:temp}\tablet.ps1" -Verb RunAs -Wait
 
 # Cleanup after Process done
 Remove-Item C:\Users\User\Desktop\install.ps1
+Remove-Item C:\Users\User\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\post-restart.bat
 Remove-Item $env:temp\tablet.ps1
